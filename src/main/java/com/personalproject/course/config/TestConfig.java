272ed9e96e2c296bcfb7ee76aaf78253b5848c21
Silva <andrevisilva@deloitte.com>
2025-03-19 11:52:49 -0300

    adding the relationship many to many between Product and OrderItem and creating entity Payment

--- src/main/java/com/personalproject/course/config/TestConfig.java
+++ src/main/java/com/personalproject/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.personalproject.course.Entities.Category;
 import com.personalproject.course.Entities.Order;
 import com.personalproject.course.Entities.OrderItem;
+import com.personalproject.course.Entities.Payment;
 import com.personalproject.course.Entities.Product;
 import com.personalproject.course.Entities.User;
 import com.personalproject.course.Entities.enums.OrderStatus;
@@ -79,7 +80,10 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
 		
+		orderRepository.save(o1);
 	}
 	
 	
